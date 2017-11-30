8d4095d1748739eaea6aee2ada9522225b531e60
Guilherme Chichanoski <guilherme.chichanoski@gmail.con>
2017-11-30 14:27:53 -0200

    CRUD Empresas

--- imports/api/empresas/empresas.js
+++ imports/api/empresas/empresas.js
@@ -2,14 +2,29 @@ import { Mongo } from 'meteor/mongo';
 import { Tracker } from 'meteor/tracker'
 import SimpleSchema from 'simpl-schema';
 
+import { validarCNPJ } from '../../utils.js'
+
 let Empresas = new Mongo.Collection('empresas');
 
 let EmpresaSchema = new SimpleSchema({
-    razaoSocial : {
-        type: String
-    },
-    cnpj : {
-        type: String
+	razaoSocial : {
+		type: String,
+		min: 3
+	},
+	cnpj : {
+		type: String,
+		custom: function() {
+            return validarCNPJ(this.value)
+		}
+	},
+	cidade : {
+		type: String,
+        regEx: /^([^0-9]*)$/,
+		min: 3
+	},
+    telefone : {
+        type: String,
+        regEx: /^[1-9]{2}\-[2-9][0-9]{7,8}$/,
     }
 }, {tracker: Tracker});
 

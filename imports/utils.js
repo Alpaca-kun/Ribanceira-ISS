2ae1ac30ca724108c8fd3f254141ea306416bbae
Guilherme Chichanoski <guilherme.chichanoski@gmail.con>
2017-12-16 17:35:04 -0200

    Adiciona validação de CPF

--- imports/utils.js
+++ imports/utils.js
@@ -47,3 +47,39 @@ export const validarCNPJ = (cnpj) => {
     if (resultado != digitos.charAt(1))
         return {name: 'cnpj', type:'notAllowed'};
 }
+
+export const validarCPF = (cpf) => {
+    cpf = cpf.replace(/[^\d]+/g,'');    
+    if(cpf == '') return {name: 'cnpj', type:'required'}; 
+    // Elimina CPFs invalidos conhecidos    
+    if (cpf.length != 11 || 
+        cpf == "00000000000" || 
+        cpf == "11111111111" || 
+        cpf == "22222222222" || 
+        cpf == "33333333333" || 
+        cpf == "44444444444" || 
+        cpf == "55555555555" || 
+        cpf == "66666666666" || 
+        cpf == "77777777777" || 
+        cpf == "88888888888" || 
+        cpf == "99999999999")
+        return {name: 'cnpj', type:'notAllowed'};       
+    // Valida 1o digito 
+    add = 0;    
+    for (i=0; i < 9; i ++)       
+        add += parseInt(cpf.charAt(i)) * (10 - i);  
+    rev = 11 - (add % 11);  
+    if (rev == 10 || rev == 11)     
+        rev = 0;    
+    if (rev != parseInt(cpf.charAt(9)))     
+        return {name: 'cnpj', type:'notAllowed'};       
+    // Valida 2o digito 
+    add = 0;    
+    for (i = 0; i < 10; i ++)        
+        add += parseInt(cpf.charAt(i)) * (11 - i);  
+    rev = 11 - (add % 11);  
+    if (rev == 10 || rev == 11) 
+        rev = 0;    
+    if (rev != parseInt(cpf.charAt(10)))
+        return {name: 'cnpj', type:'notAllowed'};       
+}

b2bd70c2f564bf7c2bab6fad7e23d8fde9175b35
Alpaca-kun <ra90558@uem.br>
2017-12-18 11:07:27 -0200

    Test func

---
+++ test/Funcionario.test.js
@@ -0,0 +1,25 @@
+import * as assert from 'assert'
+
+import Funcionarios from '../imports/api/funcionarios/funcionarios'
+
+describe('Funcionario', () =>{
+    it('Deve validar um funcionario válida', () => {
+        assert.equal(Funcionarios.simpleSchema().namedContext().validate({
+            nome : "Christian",
+            cpf : "01205266976",
+            salario : "800",
+            sindicato : "Depart. de computacao",
+            empresa : "DIN",
+        }), true)
+    })
+
+    it('Deve não validar um funcionario inválida', () => {
+        assert.equal(Funcionarios.simpleSchema().namedContext().validate({
+            nome : "Chr",
+            cpf : "01205266900",
+            salario : "-800",
+            sindicato : "aaaa",
+            empresa : "DIN",
+        }), false)
+    })
+})
